cb19bea85e71d93ef68aa766d1f38ce27a9d1279
Skywalker290 <dev.ayusshh@gmail.com>
2024-06-18 15:44:24 +0530

    latest

--- Hindi-m/train.py
+++ Hindi-m/train.py
@@ -1,12 +1,52 @@
 import os
 import pandas as pd
-import tensorflow_tts
 import tensorflow as tf
-from tensorflow_tts.inference import AutoProcessor, TFAutoModel
+from tensorflow_tts.inference import AutoProcessor
 from tensorflow_tts.configs import FastSpeechConfig
 from tensorflow_tts.models import TFFastSpeech
 from tensorflow_tts.trainers import Seq2SeqBasedTrainer
-from tensorflow_tts.datasets import CharactorDataset
+from tensorflow_tts.datasets.abstract_dataset import AbstractDataset
+
+class HindiDataset(AbstractDataset):
+    def __init__(self, metadata_filename, processor, max_seq_length=200, mel_length_threshold=0, n_files=None):
+        self.processor = processor
+        self.metadata = pd.read_csv(metadata_filename)
+        self.max_seq_length = max_seq_length
+        self.mel_length_threshold = mel_length_threshold
+        if n_files:
+            self.metadata = self.metadata[:n_files]
+
+    def get_all_data(self):
+        return self.metadata
+
+    def parse_text(self, text):
+        return self.processor.text_to_sequence(text)
+
+    def get_example(self, i):
+        sample = self.metadata.iloc[i]
+        text = sample['transcript']
+        wav_path = sample['wav_path']
+
+        # Load and preprocess wav file
+        wav = self.processor.load_wav(wav_path)
+        mel = self.processor.melspectrogram(wav)
+
+        # Preprocess text
+        text_ids = self.parse_text(text)
+
+        return {
+            "utt_id": i,
+            "input_ids": text_ids,
+            "input_lengths": len(text_ids),
+            "mel_gts": mel,
+            "mel_lengths": mel.shape[0]
+        }
+
+    def __len__(self):
+        return len(self.metadata)
+    
+    def __getitem__(self, idx):
+        return self.get_example(idx)
 
 # Define paths
 base_dir = '/content/Hindi_M'
@@ -45,7 +85,7 @@ model_save_path = './fastspeech_model_hindi'
 processor = AutoProcessor.from_pretrained("tensorspeech/tts-tacotron2-ljspeech-en")
 
 # Load the dataset
-train_dataset = CharactorDataset(
+train_dataset = HindiDataset(
     metadata_filename=csv_path,
     processor=processor,
     max_seq_length=200,
